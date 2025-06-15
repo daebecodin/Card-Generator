7cb59721fba450bcd5039b9d1f373c339b5398d3
daebecodin <136534615+daebecodin@users.noreply.github.com>
2025-06-14 20:55:32 -0700

    starting language class

--- Language.java
+++ Language.java
@@ -20,15 +20,31 @@ package assignment02PartB;
 // - Yet, curiosity for intelligence is always highly encouraged:
 // https://docs.oracle.com/en/java/javase/16/docs/api/java.base/java/util/ResourceBundle.html
 
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public final class Language {
     //
     // Static Data Fields
     //    
     private static final String defaultAlienSound = "~ ąļīæń ~ "; // Default
+    private static final Config config = new Config();
+    private static final int WIDTH = 70;
+
+    private final Map<String, String> phrases = new HashMap<>();
+
+    private static final Scanner input = new Scanner(System.in);
+
 
     //
     // Instance Data Fields
     //
+
+
+
+
 
     //
     // Constructors
@@ -39,13 +55,38 @@ public final class Language {
 
         switch (language.toLowerCase()) {
             case "alien" -> this.populateAlienPhrases();            // Supported
-            case "chinese" -> this.populateChinesePhrases();        // Future implementation
-            case "french" -> this.populateFrenchPhrases();          // Future implementation
-            case "spanish" -> this.populateSpanishPhrases();        // Future implementation
-            case "future" -> this.populateYourLanguagePhrases();    // Future implementation
+//            case "chinese" -> this.populateChinesePhrases();        // Future implementation
+//            case "french" -> this.populateFrenchPhrases();          // Future implementation
+//            case "spanish" -> this.populateSpanishPhrases();        // Future implementation
+//            case "future" -> this.populateYourLanguagePhrases();    // Future implementation
             default -> this.populateEnglishPhrases();               // Supported
         }
 
+    }
+
+    public static void displayAppHeader() {
+        System.out.println("----------------------------------------------------------------------");
+        System.out.println("------------------------- SF GIANTS THANK YOU ------------------------");
+        System.out.println("----------------------------------------------------------------------");
+        System.out.println("                                             powered by CSC 220 @ SFSU");
+    }
+
+
+    public void setLanguagePreference() {
+
+        System.out.println("Language: ");
+        String lang = input.next().trim().toLowerCase();
+        switch (lang) {
+            case "alien" -> populateAlienPhrases();
+            case "english" -> populateEnglishPhrases();
+            default -> throw new IllegalStateException("Unexpected value: " + lang);
+        }
+
+    }
+
+    private void populateAlienPhrases() {
+    }
+    private void populateEnglishPhrases() {
     }
 
     //
--- Timer.java
+++ Timer.java
@@ -28,7 +28,7 @@ public class Timer {
     public Timer() {
     }
 
-    //
+        //
     // Static Methods
     //
 
