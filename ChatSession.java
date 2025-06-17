352148ac96cf7a5839370118b60a937c8454a625
daebecodin <136534615+daebecodin@users.noreply.github.com>
2025-06-17 09:04:15 -0700

    chat session started

--- ChatSession.java
+++ ChatSession.java
@@ -15,11 +15,11 @@ package assignment02PartB;
 public final class ChatSession {
 
     //  Static Data Fields
-
+    private Club club = new Club();
+    private University university = new University();
+    private static final int WIDTH = 70;
     //
     // Instance Data Fields
-    //
-
     //
     // Constructors
     //
@@ -27,7 +27,8 @@ public final class ChatSession {
     }
 
     public ChatSession(Club club, University university) {
-
+        this.club = club;
+        this.university = university;
     }
 
     //
@@ -38,6 +39,16 @@ public final class ChatSession {
     // Additional Instance Methods
     //
     private void startChatSession() {
+        if (Language.getLocale().equalsIgnoreCase("alien")) {
+            System.out.println(Timer.timeStamp() + " - " + Language.getAlienSound());
+        } else {
+            System.out.println(Timer.timeStamp() + " - Chat Session Started");
+        }
+        System.out.println();
+        System.out.println(club.getShortName() +": Welcome to the " + club.getName().toUpperCase()+"!");
+        System.out.print("-".repeat(WIDTH));
+        System.out.println();
+
     }
     private void connectChatters() {
     }
@@ -48,9 +59,29 @@ public final class ChatSession {
     private void stopChatSession() {
     }
     public void runChatSession() {
+        startChatSession();
     }
 
     //
     // Language
     //
+
+
+    public Club getClub() {
+        return this.club;
+    }
+
+    public ChatSession setClub(Club club) {
+        this.club = club;
+        return this;
+    }
+
+    public University getUniversity() {
+        return university;
+    }
+
+    public ChatSession setUniversity(University university) {
+       this.university = university;
+        return this;
+    }
 }
