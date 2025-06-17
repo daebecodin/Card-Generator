925e2b96b4fac91240d612309920ca867cb1e555
daebecodin <136534615+daebecodin@users.noreply.github.com>
2025-06-17 09:02:31 -0700

    time stamp

--- Timer.java
+++ Timer.java
@@ -1,104 +1,71 @@
-/*
- * **********************************************
- * San Francisco State University
- * CSC 220 -  Data Structures
- * File Name: Timer.java
- * @author: Duc Ta
- * @author: <First Name> <Last Name>
- * **********************************************
- */
-
 package assignment02PartB;
-// Please organize all the given files in 1 same package
-// Please make sure to read the provided "_ListOf-PleaseDoNotChange.txt"
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
 import java.util.Locale;
 import java.util.Scanner;
 
-
 public class Timer {
 
-    //
-    // Static Data Fields
-    //
-
     private static final Scanner input = new Scanner(System.in);
-    //
-    // Instance Data Fields
-    //
-    //
-    // Constructors
-    //
-   public Timer(ZoneId zone) {
+    private ZoneId zoneId;
 
-   }
-
-
+    public Timer(ZoneId zone) {
+        this.zoneId = zone;
+    }
+    public ZoneId getZoneId() {
+        return this.zoneId;
+    }
 
     public static Timer setTimeZonePreference() {
-
-
         while (true) {
-            String timeZone;
-            System.out.print("TimeZone: ");
+            if (Language.getLocale().equalsIgnoreCase("alien")) {
+                System.out.print(Language.getAlienSound() + ": ");
+            } else {
+                System.out.print("TimeZone: ");
+            }
             String in = input.next().trim().toUpperCase();
-            ZoneId zone;
-
-
             switch (in) {
-            case "EST" -> {
-                zone = ZoneId.of("America/New_York");
-            }
-
-            case "PST" -> {
-                zone = ZoneId.of("America/Los_Angeles");
-            }
-
-            default -> {
-                System.out.println("Invalid Zone: " + in);
-                continue;
+                case "EST":
+                    return new Timer(ZoneId.of("America/New_York"));
+                case "PST":
+                    return new Timer(ZoneId.of("America/Los_Angeles"));
+                default:
+                    System.out.println("Invalid Zone: " + in);
             }
         }
-
-        return new Timer(zone);
     }
-   }
+    
+    public static String timeStamp() {
+
+
+        ZonedDateTime zdt = ZonedDateTime.now(ZoneId.of(String.valueOf(ZoneId.systemDefault())));
+        DateTimeFormatter dtf = new DateTimeFormatterBuilder()
+                .appendPattern("yyyy/MM/dd hh:mm:ss ") // desired format for date and time
+                .appendLiteral("[") // append a starting bracket
+                .appendValue(ChronoField.MILLI_OF_SECOND, 4) // in the bracket we want to add milliseconds in time
+                .appendLiteral("ms] ") // close brackets and add text inside
+                .appendPattern("a z") // return am/pm and the zone
+                .toFormatter(); // format
+
+//        System.out.println(output);
+        
+        return zdt.format(dtf);
+    }
 
     public String getTimeZoneFormatted() {
+        // --- NEW: This method is now language-aware ---
+        if ("alien".equals(Language.getLocale())) {
+            return Language.getAlienSound();
+        }
 
-        ZonedDateTime zdt = ZonedDateTime.now(); // current time zone
-
+        ZonedDateTime zdt = ZonedDateTime.now(this.zoneId);
         String fullName = zdt.format(DateTimeFormatter.ofPattern("zzzz", Locale.ENGLISH));
-
-        boolean isDst = zdt.getZone().getRules().isDaylightSavings(zdt.toInstant()); // returns a boolean of if the zone is in dst
-        String status  = isDst ? "in Daylight Saving Time" : "not in Daylight Saving Time"; // based on that value , print with according text
-        return String.format("%s %s", fullName, status); // formated string
-
-
+        boolean isDst = zdt.getZone().getRules().isDaylightSavings(zdt.toInstant());
+        String status = isDst ? "in Daylight Saving Time" : "not in Daylight Saving Time";
+        return String.format("%s %s", fullName, status);
     }
-
-
-
-    //
-    // Static Methods
-    //
-
-    //
-    // Additional Static Methods
-    //
-
-    //
-    // Instance Methods
-    //
-
-    //
-    // Additional Instance Methods
-    //
-
-    //
-    // Language
-    //
 }
